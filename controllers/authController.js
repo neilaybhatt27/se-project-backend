e3eadf033aba135f3bc1ff3a8209946b64e27516
Neilay Bhatt <neilaybhatt27@gmail.com>
2023-11-21 18:28:48 +0530

    Login route added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 const User = require("../models/users");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
+
+const secretKey = process.env.JWT_SECRET;
 
 //User registration
 exports.register = async (req, res) => {
@@ -21,3 +24,25 @@ exports.register = async (req, res) => {
         res.status(500).json({message: err.message});
     }
 };
+
+//User login
+exports.login = async (req, res) => {
+    try {
+        console.log(req.body);
+        const user = await User.findOne({email: req.body.email});
+        if(!user){
+            return res.status(400).json({message: "User Not Found!"});
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if(!validPassword){
+            return res.status(400).json({message: "Invalid password!"});
+        }
+
+        //Generate token
+        const token = jwt.sign({_id: user._id}, secretKey, {expiresIn: "12h"});
+        res.header("auth-token", token).json({token, _id: user._id});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}
