66b55d57ada8a21139c06b25fa3095ebe0bee7b4
unknown <ruchasanghvi@gmail.com>
2023-12-07 18:25:22 +0530

    Community error solved

--- models/community.js
+++ models/community.js
@@ -1,64 +1,24 @@
-const mongoose = require("mongoose");
-const fs = require("fs");
-
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const defaultImageData = fs.readFileSync("./defaults/default-img.jpg");
+const CommunitySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required:false
+  },
+  createdby: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
 
-const BookSchema = new Schema({
-
-    userid : {
-        type: Schema.Types.ObjectId,
-        ref : 'User',
-        required:true
-    },
-
-    title : {
-        type: String,
-        required : true
-    },
-
-    description : {
-        type: String,
-        required : true
-    },
-
-    author: {
-        type : String,
-        required: true
-    },
-
-    genre : {
-        type : String,
-        required: false
-    },
-
-    status : {
-        type : Boolean,
-        default : true
-    },
-
-    bookimage: {
-        data: {
-            type: Buffer,
-            default: defaultImageData
-        },
-        contentType: {
-            type: String,
-            default: "default-image.jpg"
-        }
-    },
-    
-    location: {
-        type: {
-                type: String,
-                default: 'Point',
-            },
-        coordinates: [Number]
-    },
-    
-})
-
-BookSchema.index({ location: '2dsphere' });
-
-module.exports = mongoose.model("Book", BookSchema);
+module.exports = mongoose.model('Community', CommunitySchema);
